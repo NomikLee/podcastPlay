5feb5a9cbe0b2a8c38d446ed41cd178eeb596a7d
NomikLee <nomiklee@icloud.com>
2023-08-12 21:47:08 +0800

    two round code

--- podcastPlay/Model/podcasList.swift
+++ podcastPlay/Model/podcasList.swift
@@ -21,7 +21,15 @@ let podcastDatas =
 [
     podcastInit(podcastImage: "蔡阿嘎", podcastName: "蔡阿嘎543", podcastPeopleName: "蔡阿嘎", podcastWebUrl: "https://rss.soundon.fm/rssf/60f9a882-edee-4e5c-8fd1-91814ad90fcd/feedurl/c0cc4a70-e7a4-4772-8f1f-608336981ba4/rssFileVip.mp3?timestamp=1691734168217"),
     podcastInit(podcastImage: "李兆華", podcastName: "兆華與股惑仔", podcastPeopleName: "李兆華", podcastWebUrl: "https://rss.soundon.fm/rssf/91be014b-9f55-4bf3-a910-b232eda82d11/feedurl/21fdad71-1617-454d-abba-4e23489efbf4/rssFileVip.mp3?timestamp=1691744221652"),
-    podcastInit(podcastImage: "謝孟恭", podcastName: "Gooaye 股癌", podcastPeopleName: "謝孟恭", podcastWebUrl: "https://chtbl.com/track/4B4E1D/rss.soundon.fm/rssf/954689a5-3096-43a4-a80b-7810b219cef3/feedurl/8c524a1b-c3a5-4f32-8aad-f6eb98e0d473/rssFileVip.mp3?timestamp=1691578173814"),
-    podcastInit(podcastImage: "KISS RADIO", podcastName: "KISS RADIO", podcastPeopleName: "KISS RADIO", podcastWebUrl: "https://rss.soundon.fm/rssf/5a4b4d13-9935-43d2-99f7-d7456d9e7f76/feedurl/0f45c589-8dea-4dee-9c37-8bb3dbecdc32/rssFileVip.mp3?timestamp=1691742310865")
+    podcastInit(podcastImage: "Gooaye 股癌", podcastName: "Gooaye 股癌", podcastPeopleName: "謝孟恭", podcastWebUrl: "https://chtbl.com/track/4B4E1D/rss.soundon.fm/rssf/954689a5-3096-43a4-a80b-7810b219cef3/feedurl/8c524a1b-c3a5-4f32-8aad-f6eb98e0d473/rssFileVip.mp3?timestamp=1691578173814"),
+    podcastInit(podcastImage: "KISS RADIO", podcastName: "KISS RADIO", podcastPeopleName: "KISS RADIO", podcastWebUrl: "https://rss.soundon.fm/rssf/5a4b4d13-9935-43d2-99f7-d7456d9e7f76/feedurl/0f45c589-8dea-4dee-9c37-8bb3dbecdc32/rssFileVip.mp3?timestamp=1691742310865"),
+    podcastInit(podcastImage: "唐綺陽", podcastName: "唐陽雞酒屋", podcastPeopleName: "唐綺陽", podcastWebUrl: "https://chrt.fm/track/DFD86F/https://backend.endpoints.firstory-709db.cloud.goog/play.mp3?url=https%3A%2F%2Fd3mww1g1pfq2pt.cloudfront.net%2FRecord%2Fckga7ibs77fgl0875bxwgl0y0%2Fcll67ilrh00sx01wpgdeg8k1f.mp3%3Fv%3D1691735356073"),
+    podcastInit(podcastImage: "風傳媒", podcastName: "下班經濟學", podcastPeopleName: "風傳媒", podcastWebUrl: "https://chtbl.com/track/D77AD4/rss.soundon.fm/rssf/208dfd5b-d11b-4236-ab87-d8f0bf01d7d0/feedurl/e70553c4-5d8f-4a22-b730-0936cf41d1cc/rssFileVip.mp3?timestamp=1691804535490"),
+    podcastInit(podcastImage: "曲博", podcastName: "曲博科技教室", podcastPeopleName: "曲博", podcastWebUrl: "https://rss.soundon.fm/rssf/3067c238-9028-48fa-be16-ae37578ed148/feedurl/47c920fc-bad4-4c7e-b351-62e82a9bcece/rssFileVip.mp3?timestamp=1690664175713"),
+    podcastInit(podcastImage: "隱者", podcastName: "股市隱者", podcastPeopleName: "隱者", podcastWebUrl: "https://chrt.fm/track/C5649A/rss.soundon.fm/rssf/eb9e90a8-a889-425b-8855-4cf8cdf92c73/feedurl/d655ef45-e0c8-403e-93a3-386bbbe2dc55/rssFileVip.mp3?timestamp=1691716666657"),
+    podcastInit(podcastImage: "老王", podcastName: "王老先聲", podcastPeopleName: "老王", podcastWebUrl: "https://rss.soundon.fm/rssf/c27d634c-7d80-4924-ab3d-7655123d0fe3/feedurl/89c1482c-e6fb-4a24-b82c-e1ca745d8fed/rssFileVip.mp3?timestamp=1691579277503"),
+    podcastInit(podcastImage: "TheDoDoMen", podcastName: "跳脫Do式圈", podcastPeopleName: "TheDoDoMen", podcastWebUrl: "https://chrt.fm/track/6197DE/rss.soundon.fm/rssf/22505944-fec2-4417-b277-649ce5d3a491/feedurl/3db7aa9c-107f-4e32-842f-0461dd481e81/rssFileVip.mp3?timestamp=1691364514291"),
+    
+    
 ]
 
--- podcastPlay/ViewController/podcasTableViewController.swift
+++ podcastPlay/ViewController/podcasTableViewController.swift
@@ -12,7 +12,8 @@ class podcasTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .black
+        tableView.backgroundView = UIImageView(image: UIImage(named: "背景9"))
+        tableView.backgroundView?.alpha = 0.5
     }
     
 
@@ -30,10 +31,11 @@ class podcasTableViewController: UITableViewController {
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "podcasTableViewCell", for: indexPath) as! podcasTableViewCell
         
-        cell.backgroundColor = .orange
-        cell.layer.borderWidth = 3
-        cell.layer.cornerRadius = 100
-        cell.layer.borderColor = CGColor(red: 0, green: 0, blue: 1, alpha: 2)
+        
+        cell.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+        cell.layer.borderWidth = 10
+        cell.layer.cornerRadius = 240
+        cell.layer.borderColor = CGColor(red: 241/255, green: 90/255, blue: 34/255, alpha: 2)
         
         let podcasData = podcastDatas[indexPath.row]
         cell.podcasNameLable.text = podcasData.podcastName
